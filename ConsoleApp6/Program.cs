5b9ca8ee28e66d087a88a12d7fcd4c2bc451015c
FaneNohman <kingofthepixels1@gmail.com>
2023-03-30 19:01:10 +0600

    first commit

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,43 @@
+﻿
+using Microsoft.Win32;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string subKeyName = "CustomSubKeyForCurrentUser";
+            Console.WriteLine("Registry");
+            RegistryKey CurrentUser = Registry.CurrentUser;
+            RegistryKey newSubKey = CurrentUser.CreateSubKey(subKeyName);
+
+            newSubKey.SetValue("isTestValueSet", "true");
+            newSubKey.Close();
+            CurrentUser.Close();
+
+            Console.WriteLine("Press to continue");
+            Console.ReadLine();
+            CurrentUser= Registry.CurrentUser;
+            newSubKey = CurrentUser.OpenSubKey(subKeyName,true);
+
+            newSubKey.DeleteValue("isTestValueSet");
+            CurrentUser.DeleteSubKey(subKeyName);
+
+            CurrentUser.Close();
+
+        }
+
+        class ForCleaning
+        {
+            public ForCleaning()
+            {
+
+            }
+            ~ForCleaning()
+            {
+
+            }
+        }
+    }
+}
